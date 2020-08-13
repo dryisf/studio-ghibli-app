dcd8cbda07c0fadb1d8e2aad5c7e699eee84ffa2
dryisf <daryl.issoufi@gmail.com>
2020-08-13 10:26:50 +0200

    retrait modules inutilisés

--- src/FilmDetails.jsx
+++ src/FilmDetails.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React  from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimesCircle, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
 const FilmDetails = ({data, onClose, top}) => {
     return (
